a96e6519bdeeaa864062b0ee56e418fb49266e7a
MartinaFusik <martina.fusikova@gmail.com>
2022-08-06 13:55:16 +0200

    Start

--- election-scraper.py
+++ election-scraper.py
@@ -7,13 +7,14 @@ discord: Martina_F#2319
 import bs4
 import csv
 import requests
+import pandas as pd
 
 def main():
     url = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7103"
     odpoved = ziskej_parsovanou_odpoved(url)
-    zahlavi, data = rozdel_zahlavi_a_data(vyber_jen_tr_tagy(odpoved))
-    print(data)
-
+    zahlavi, zdroj = rozdel_zahlavi_a_data(vyber_jen_tr_tagy(odpoved))
+    final_radek = rozdeleni_zdroje(zdroj)
+    print(zahlavi, final_radek)
 def ziskej_parsovanou_odpoved(url: str):
     '''Získej rozdělenou odpověď na požadavek get.'''
     return bs4.BeautifulSoup(requests.get(url).text, features="html.parser")
@@ -27,6 +28,10 @@ def rozdel_zahlavi_a_data(all_tr: bs4.element.ResultSet):
     header: list = header_tag.get_text().splitlines()[1:]
     return header, data
 
+def rozdeleni_zdroje(obsah: bs4.element.ResultSet):
+    headers, *others = obsah
+    data: list = headers.get_text().splitlines()[1:3]
+    return data
 
 if __name__ == "__main__":
     main()
