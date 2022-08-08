a4b0f3760c699a0a7a63305a392baa44509dcd80
MartinaFusik <martina.fusikova@gmail.com>
2022-08-08 21:32:47 +0200

    Edit - table

--- election-scraper.py
+++ election-scraper.py
@@ -4,34 +4,51 @@ author: Martina Fúsiková
 email: martina.fusikova@gmail.com
 discord: Martina_F#2319
 """
-import bs4
-import csv
+
 import requests
+import bs4
 import pandas as pd
 
 def main():
     url = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7103"
     odpoved = ziskej_parsovanou_odpoved(url)
-    zahlavi, zdroj = rozdel_zahlavi_a_data(vyber_jen_tr_tagy(odpoved))
-    final_radek = rozdeleni_zdroje(zdroj)
-    print(zahlavi, final_radek)
+    cisla_obce = sloupec_cisla(odpoved)
+    nazev_obce = sloupec_nazev(odpoved)
+    odkazy = data_z_odkazu(odpoved)
+    df = pd.DataFrame({"Číslo obce": cisla_obce,
+                       "Název obce": nazev_obce,
+                       "Odkazy": odkazy}
+                      )
+    df.to_csv('items.csv', encoding='utf-8')
+
 def ziskej_parsovanou_odpoved(url: str):
     '''Získej rozdělenou odpověď na požadavek get.'''
     return bs4.BeautifulSoup(requests.get(url).text, features="html.parser")
 
-def vyber_jen_tr_tagy(soup: bs4.BeautifulSoup) -> bs4.element.ResultSet:
-    '''Ze zdrojového kódu vybere všechny tagy "tr"'''
-    return soup.findAll("tr")
+def ziskej_informace_z_table(obsah):
+    table = obsah.find("table", id="t%sa%")
+    return table
 
-def rozdel_zahlavi_a_data(all_tr: bs4.element.ResultSet):
-    header_tag, *data = all_tr[1:]
-    header: list = header_tag.get_text().splitlines()[1:]
-    return header, data
+def sloupec_cisla(data):
+    cisla = []
+    for i in data.find_all("td", {"class": "cislo"}):
+        cislo = i.text
+        cisla.append(cislo)
+    return cisla
 
-def rozdeleni_zdroje(obsah: bs4.element.ResultSet):
-    headers, *others = obsah
-    data: list = headers.get_text().splitlines()[1:3]
-    return data
+def sloupec_nazev(data):
+    nazev = []
+    for i in data.find_all("td", {"class": "overflow_name"}):
+        obec = i.text
+        nazev.append(obec)
+    return nazev
+
+def data_z_odkazu(data):
+    links = []
+    for i in data.find_all("td", {"class": "cislo"}):
+        for x in i.find_all("a"):
+            links.append(x.get("href"))
+    return links
 
 if __name__ == "__main__":
     main()
